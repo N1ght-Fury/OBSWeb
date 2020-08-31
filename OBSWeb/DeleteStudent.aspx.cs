eff1aafeb72e2c4c3be6606e4f7eb0ec5c97e68b
N1ght-Fury <44553433+N1ght-Fury@users.noreply.github.com>
2020-08-31 13:49:25 +0300

    Initial Commit

---
+++ OBSWeb/DeleteStudent.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OBSWeb
+{
+    public partial class DeleteStudent : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(Request.QueryString["OGRID"].ToString());
+            
+            DataSet1TableAdapters.TBL_OGRENCITableAdapter dt = new DataSet1TableAdapters.TBL_OGRENCITableAdapter();
+            dt.DeleteStudent(id);
+            Response.Redirect("Default.aspx");
+        }
+    }
+}
